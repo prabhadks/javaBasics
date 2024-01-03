abcdfedd3f6fc7bae8193f2e0d9b7e1bbfcf2454
PrabhaDodda <prabhadoddalearnings@gmail.com>
2024-01-03 22:26:08 +0000

    First Program

---
+++ FirstProgram/src/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * This Program is to print the Hello World Application in Console
+ * @author Prabhavathi Dodda
+ */
+public class HelloWorld {
+    /**
+     * Main method
+     * @param args String arguments
+     */
+    public static void main(String[] args){
+       /*
+       This is critical function that JVM looks to initate the application
+       Do not delete main function
+        */
+        System.out.println("Hello World Application");
+        //To Do - Add code in future
+    }
+}
